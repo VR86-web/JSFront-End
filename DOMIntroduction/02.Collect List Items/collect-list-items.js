e4bd4502fe608a5cf1268e6d09e4c074b956646c
VR86-web <145848782+VR86-web@users.noreply.github.com>
2026-07-25 21:05:18 +0200

    added solution for exercise 2 dom introduction

--- DOMIntroduction/02.Collect List Items/collect-list-items.js
+++ DOMIntroduction/02.Collect List Items/collect-list-items.js
@@ -1,3 +1,11 @@
 function extractText() {
-    // TODO
+    let liItems = document.getElementsByTagName('li')
+    let textareaEl = document.querySelector('#result')
+
+    for(liItem of liItems) {
+        let liText = liItem.textContent
+        textareaEl.textContent += liText + '\n'
+
+    }
+
 }
