995aa2063e82fa00a10b1c8298d50d342d8f6d0f
VR86-web <145848782+VR86-web@users.noreply.github.com>
2026-07-26 12:42:59 +0200

    added solution for  first exercise

--- ExercisesDOMIntroduction/01. Subtraction/subtraction.js
+++ ExercisesDOMIntroduction/01. Subtraction/subtraction.js
@@ -1,3 +1,11 @@
-function subtract() {
-    //TODO
+function subtract(num1, num2) {
+    const numEl = document.getElementById('firstNumber')
+    const num2El = document.querySelector('#secondNumber')
+    const result = document.getElementById('result')
+
+    let firstNum = Number(numEl.value)
+    let secondNum = Number(num2El.value)
+
+    result.textContent = firstNum - secondNum
+
 }
