4d1671d5fa8bf1a356f5ddd2b52b13a4c182d730
VR86-web <145848782+VR86-web@users.noreply.github.com>
2026-07-27 23:50:06 +0200

    added more solutions

--- ExercisesDOMIntroduction/05. Table - Search Engine/table-search-engine.js
+++ ExercisesDOMIntroduction/05. Table - Search Engine/table-search-engine.js
@@ -1,3 +1,17 @@
 function solve() {
-   //TODO
+   const tdEl = document.querySelectorAll('tbody tr')
+   const searchEl = document.getElementById('searchField').value.toLowerCase()
+
+   if (!searchEl) return;
+
+   for(let element of tdEl){
+      element.classList.remove('select')
+      if(element.textContent.toLowerCase().includes(searchEl)){
+         element.classList.add('select')
+      }
+         
+   }
+
+   document.getElementById('searchField').value = '';
+   
 }
