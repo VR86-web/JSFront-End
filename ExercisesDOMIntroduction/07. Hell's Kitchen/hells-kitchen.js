6b9c2cab632789cc6337156f6a48a9cb1777b6dd
VR86-web <145848782+VR86-web@users.noreply.github.com>
2026-07-28 21:04:19 +0200

    added more solutions

--- ExercisesDOMIntroduction/07. Hell's Kitchen/hells-kitchen.js
+++ ExercisesDOMIntroduction/07. Hell's Kitchen/hells-kitchen.js
@@ -1,3 +1,76 @@
 function solve() {
-    //TODO
+    const input = document.querySelector('#inputs textarea').value;
+    const bestRestaurantOutput = document.querySelector('#bestRestaurant p');
+    const workersOutput = document.querySelector('#workers p');
+
+    const restaurantsInput = JSON.parse(input);
+
+    const restaurants = {};
+
+    for (let restaurantInfo of restaurantsInput) {
+
+        let [restaurantName, workersInfo] = restaurantInfo.split(' - ');
+
+        if (!restaurants[restaurantName]) {
+            restaurants[restaurantName] = {
+                workers: [],
+                avgSalary: 0,
+                bestSalary: 0
+            };
+        }
+
+        let workers = workersInfo.split(', ');
+
+        for (let worker of workers) {
+            let [workerName, salary] = worker.split(' ');
+
+            restaurants[restaurantName].workers.push({
+                name: workerName,
+                salary: Number(salary)
+            });
+        }
+
+        let totalSalary = 0;
+        let bestSalary = 0;
+
+        for (let worker of restaurants[restaurantName].workers) {
+            totalSalary += worker.salary;
+
+            if (worker.salary > bestSalary) {
+                bestSalary = worker.salary;
+            }
+        }
+
+        restaurants[restaurantName].avgSalary =
+            totalSalary / restaurants[restaurantName].workers.length;
+
+        restaurants[restaurantName].bestSalary = bestSalary;
+    }
+
+    let bestRestaurantName = '';
+    let highestAverageSalary = 0;
+
+    for (let restaurantName in restaurants) {
+
+        if (restaurants[restaurantName].avgSalary > highestAverageSalary) {
+
+            highestAverageSalary = restaurants[restaurantName].avgSalary;
+            bestRestaurantName = restaurantName;
+        }
+    }
+
+    let bestRestaurant = restaurants[bestRestaurantName];
+
+    bestRestaurant.workers.sort((a, b) => b.salary - a.salary);
+
+    bestRestaurantOutput.textContent =
+        `Name: ${bestRestaurantName} Average Salary: ${bestRestaurant.avgSalary.toFixed(2)} Best Salary: ${bestRestaurant.bestSalary.toFixed(2)}`;
+
+    let workersText = '';
+
+    for (let worker of bestRestaurant.workers) {
+        workersText += `Name: ${worker.name} With Salary: ${worker.salary} `;
+    }
+
+    workersOutput.textContent = workersText.trim();
 }
