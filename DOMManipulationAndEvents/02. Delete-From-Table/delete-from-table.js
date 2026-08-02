edb748055f127e10c20ae543e514816f6d8c2ac3
VR86-web <145848782+VR86-web@users.noreply.github.com>
2026-08-02 13:12:31 +0200

    added more solutions

--- DOMManipulationAndEvents/02. Delete-From-Table/delete-from-table.js
+++ DOMManipulationAndEvents/02. Delete-From-Table/delete-from-table.js
@@ -1,3 +1,15 @@
 function deleteByEmail() {
-    //TODO
+    const input = document.querySelector('label input').value
+    const rowElement = document.querySelectorAll('#customers tbody tr')
+    const resultElement = document.getElementById('result')
+
+    rowElement.forEach(element => {
+        const emailContent = element.querySelector('td:nth-child(2)');
+        if (emailContent && emailContent.textContent === input) {
+            element.remove();
+        }else{
+            resultElement.textContent = 'Not found.'
+        }
+    });
+    
 }
--- DOMManipulationAndEvents/03. Add-Delete/add-delete.js
+++ DOMManipulationAndEvents/03. Add-Delete/add-delete.js
@@ -1,3 +1,21 @@
 function addItem() {
-    // TODO
+    let inputEl = document.getElementById('newItemText').value
+    const ulItemsElement = document.getElementById('items')
+    const liElement = document.createElement('li')
+    const aElement = document.createElement('a')
+
+    liElement.textContent = inputEl
+    aElement.href = '#'
+    aElement.textContent = '[Delete]'
+    liElement.appendChild(aElement)
+
+    ulItemsElement.append(liElement)
+
+    aElement.addEventListener('click', deleteAElement)
+
+    function deleteAElement(e){
+        e.preventDefault()
+        e.target.parentElement.remove()
+    }
+
 }
