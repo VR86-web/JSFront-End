b87f013af476b0bd08a65fd068b7fec6236f8ee8
VR86-web <145848782+VR86-web@users.noreply.github.com>
2026-08-02 20:30:40 +0200

    added more solutions

--- DOMManipulationAndEvents/04. Mouse-Gradient/mouse-gradient.js
+++ DOMManipulationAndEvents/04. Mouse-Gradient/mouse-gradient.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    //TODO
+    const gradientBox = document.getElementById('gradient')
+    const resultElement = document.getElementById('result')
+
+    gradientBox.addEventListener('mousemove', mouseMoveHandler)
+
+    function mouseMoveHandler(e) {
+        const cursoreDistanceFromLeft = e.offsetX
+        const gradientBoxWidth = e.target.clientWidth
+
+        const percent = Math.floor(cursoreDistanceFromLeft / gradientBoxWidth * 100)
+
+        resultElement.textContent = `${percent}%`
+    }
+
+    
 }
--- DOMManipulationAndEvents/05. Highlight-Active/hightlight-active.js
+++ DOMManipulationAndEvents/05. Highlight-Active/hightlight-active.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', focused);
 
 function focused() {
-    //TODO
+    const allInputElements = document.querySelectorAll('input[type="text"]')
+
+    for(const inputElement of allInputElements){
+        inputElement.addEventListener('focus', addClass)
+        inputElement.addEventListener('blur', removeClass)
+
+    }
+
+    function addClass(e){
+            e.target.parentElement.classList.add('focused')
+        }
+
+    function removeClass(e){
+            e.target.parentElement.classList.remove('focused')
+        }
+
 }
