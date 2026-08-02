7b28d3ece6ffab259132bf1b5c12a4e6976824e6
VR86-web <145848782+VR86-web@users.noreply.github.com>
2026-08-02 11:44:40 +0200

    added solution for exercise 1

--- DOMManipulationAndEvents/01. List-Of-Items/list-of-items.js
+++ DOMManipulationAndEvents/01. List-Of-Items/list-of-items.js
@@ -1,3 +1,10 @@
 function addItem() {
-    //TODO
+    let inputEl = document.getElementById('newItemText').value
+    const ulItemsElement = document.getElementById('items')
+    const liElement = document.createElement('li')
+
+    liElement.textContent = inputEl
+
+    ulItemsElement.append(liElement)
+
 }
