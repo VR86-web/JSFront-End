ab060515cd3f2c286250da64686e0d1537995727
VR86-web <145848782+VR86-web@users.noreply.github.com>
2026-08-04 22:20:14 +0200

    added more solutions

--- DOMManipulationAndEventsExercises/01. Sections/sections.js
+++ DOMManipulationAndEventsExercises/01. Sections/sections.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   // TODO
+   const formEl = document.getElementById('task-input')
+   const contentEl = document.getElementById('content')
+
+   formEl.addEventListener('submit', (e) =>{
+      e.preventDefault()
+      contentEl.innerHTML = ''
+
+      const inputEl = document.querySelector('input[type="text"]').value.split(', ')
+
+      inputEl.forEach(element => {
+         const divEL = document.createElement('div')
+         const pEl = document.createElement('p')
+
+         pEl.textContent = element
+
+         divEL.append(pEl)
+
+         pEl.style.display = 'none'
+
+         divEL.addEventListener('click', (e) => {
+            e.target.querySelector('p').style.display = 'block'
+         })
+
+         contentEl.append(divEL)
+      });
+
+   })
 }
--- DOMManipulationAndEventsExercises/02. Time-Converter/time-converter.js
+++ DOMManipulationAndEventsExercises/02. Time-Converter/time-converter.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', solve);
 
-function solve() {
-    //TODO
+function attachEventsListeners() {
+    const days = document.getElementById('days-input');
+    const hours = document.getElementById('hours-input');
+    const minutes = document.getElementById('minutes-input');
+    const seconds = document.getElementById('seconds-input');
+
+    function convert(totalSeconds) {
+        days.value = totalSeconds / 86400;
+        hours.value = totalSeconds / 3600;
+        minutes.value = totalSeconds / 60;
+        seconds.value = totalSeconds;
+    }
+
+    document.getElementById('daysBtn').addEventListener('click', () => {
+        convert(Number(days.value) * 86400);
+    });
+
+    document.getElementById('hoursBtn').addEventListener('click', () => {
+        convert(Number(hours.value) * 3600);
+    });
+
+    document.getElementById('minutesBtn').addEventListener('click', () => {
+        convert(Number(minutes.value) * 60);
+    });
+
+    document.getElementById('secondsBtn').addEventListener('click', () => {
+        convert(Number(seconds.value));
+    });
 }
