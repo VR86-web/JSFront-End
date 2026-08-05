a1b9265b215d3250f0a0d1fc524eedb1c885f63b
VR86-web <145848782+VR86-web@users.noreply.github.com>
2026-08-05 20:33:12 +0200

    added more solutions

--- DOMManipulationAndEventsExercises/03. Locked-Profile/locked-profile.js
+++ DOMManipulationAndEventsExercises/03. Locked-Profile/locked-profile.js
@@ -1,5 +1,27 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const profileEl = document.querySelectorAll('.profile');
+
+    profileEl.forEach(element => {
+        const btn = element.querySelector('button');
+        const hiddenFieldsDiv = element.querySelector('.hidden-fields');
+        const unlockRadio = element.querySelector('input[id$="Unlock"]');
+
+        btn.addEventListener('click', () => {
+
+            if (!unlockRadio.checked) {
+                return;
+            }
+
+            if (btn.textContent === 'Show more') {
+                hiddenFieldsDiv.classList.remove('active');
+                btn.textContent = 'Hide it';
+            } else {
+                hiddenFieldsDiv.classList.add('active');
+                btn.textContent = 'Show more';
+            }
+
+        });
+    });
 }
